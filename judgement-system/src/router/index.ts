2513c15e6743104d02b27deec0ccea8285b37503
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2021-12-05 22:24:45 +0700

    login page sementara

--- judgement-system/src/router/index.ts
+++ judgement-system/src/router/index.ts
@@ -6,7 +6,7 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
+    path: '/home/',
     name: 'Home',
     component: Home,
     children: [
@@ -17,19 +17,54 @@ const routes: Array<RouteConfig> = [
           {
             path: 'setting',
             name: 'Admin Setting',
-            component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+            component: () => import('@/views/ADMIN/setting.vue'),
+          },
+        ],
+      },
+      {
+        path: 'judge/',
+        name: 'Judges',
+        children: [
+          {
+            path: 'point',
+            name: 'Judges Point',
+            component: () => import('@/views/JUDGES/point.vue'),
+          },
+        ],
+      },
+      {
+        path: 'user/',
+        name: 'User',
+        children: [
+          {
+            path: 'registration',
+            name: 'User Registration',
+            component: () => import('@/views/USER/registration.vue'),
+          },
+          {
+            path: 'dashboard',
+            name: 'User Dashboard',
+            component: () => import('@/views/USER/dashboard.vue'),
           },
         ],
       },
     ],
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('@/views/login.vue'),
+  },
+  {
+    path: '/schedule',
+    name: 'Schedule',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('@/views/schedule.vue'),
   },
 ];
 
