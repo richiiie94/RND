ee3480af136e6afdc9ca05c43bb390bbcb57b2cf
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2021-12-12 14:23:41 +0700

    Update

--- FRONTEND/judgement-system/src/components/Login.vue
+++ FRONTEND/judgement-system/src/components/Login.vue
@@ -1,88 +1,96 @@
 <template>
   <v-app inspire>
-    <v-card>
-      <validation-observer ref="observer" v-slot="{ invalid }">
-        <form @submit.prevent="submit">
-          <validation-provider
-            v-slot="{ errors }"
-            name="Name"
-            rules="required|max:10"
-          >
-            <v-text-field
-              v-model="name"
-              :counter="10"
-              :error-messages="errors"
-              label="Name"
-              required
-            ></v-text-field>
-          </validation-provider>
+    <v-container fluid>
+      <v-row align="center" justify="center" dense>
+        <v-col cols="12" sm="8" md="4" lg="4">
+          <v-card style.border-radius="25%" elevation="5">
+            <v-card-text>
+              <validation-observer ref="observer" v-slot="{ invalid }">
+                <form @submit.prevent="submit">
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="Name"
+                    rules="required|max:10"
+                  >
+                    <v-text-field
+                      v-model="name"
+                      :counter="10"
+                      :error-messages="errors"
+                      label="Name"
+                      required
+                    ></v-text-field>
+                  </validation-provider>
 
-          <validation-provider
-            v-slot="{ errors }"
-            name="Phone Number"
-            :rules="{
-              required: true,
-              digits: 7,
-              regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$',
-            }"
-          >
-            <v-text-field
-              v-model="phoneNumber"
-              :counter="7"
-              :error-messages="errors"
-              label="Phone Number"
-              required
-            ></v-text-field>
-          </validation-provider>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="Phone Number"
+                    :rules="{
+                      required: true,
+                      digits: 7,
+                      regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$',
+                    }"
+                  >
+                    <v-text-field
+                      v-model="phoneNumber"
+                      :counter="7"
+                      :error-messages="errors"
+                      label="Phone Number"
+                      required
+                    ></v-text-field>
+                  </validation-provider>
 
-          <validation-provider
-            v-slot="{ errors }"
-            name="E-mail"
-            rules="required|email"
-          >
-            <v-text-field
-              v-model="email"
-              :error-messages="errors"
-              label="E-mail"
-              required
-            ></v-text-field>
-          </validation-provider>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="E-mail"
+                    rules="required|email"
+                  >
+                    <v-text-field
+                      v-model="email"
+                      :error-messages="errors"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
+                  </validation-provider>
 
-          <validation-provider
-            v-slot="{ errors }"
-            name="Selection"
-            rules="required"
-          >
-            <v-select
-              v-model="select"
-              :items="items"
-              :error-messages="errors"
-              label="Select"
-              data-vv-name="select"
-              required
-            ></v-select>
-          </validation-provider>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="Selection"
+                    rules="required"
+                  >
+                    <v-select
+                      v-model="select"
+                      :items="items"
+                      :error-messages="errors"
+                      label="Select"
+                      data-vv-name="select"
+                      required
+                    ></v-select>
+                  </validation-provider>
 
-          <validation-provider
-            v-slot="{ errors }"
-            rules="required"
-            name="Checkbox"
-          >
-            <v-checkbox
-              v-model="checkbox"
-              :error-messages="errors"
-              value="1"
-              label="Option"
-              type="checkbox"
-              required
-            ></v-checkbox>
-          </validation-provider>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    rules="required"
+                    name="Checkbox"
+                  >
+                    <v-checkbox
+                      v-model="checkbox"
+                      :error-messages="errors"
+                      value="1"
+                      label="Option"
+                      type="checkbox"
+                      required
+                    ></v-checkbox>
+                  </validation-provider>
 
-          <v-btn class="mr-4" type="submit" :disabled="invalid"> submit </v-btn>
-          <v-btn @click="clear"> clear </v-btn>
-        </form>
-      </validation-observer>
-    </v-card>
+                  <v-btn class="mr-4" type="submit" :disabled="invalid"> submit </v-btn>
+                  <v-btn @click="clear"> clear </v-btn>
+                </form>
+              </validation-observer>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -118,3 +126,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+</style>
