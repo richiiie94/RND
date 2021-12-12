2ea2a1da4f8f5a9ddd8974fdf62a9c3361926470
richiiie94 <47447885+richiiie94@users.noreply.github.com>
2021-12-12 14:04:25 +0700

    Update

--- FRONTEND/judgement-system/src/App.vue
+++ FRONTEND/judgement-system/src/App.vue
@@ -7,6 +7,35 @@
 <script lang="ts">
 import Vue from 'vue';
 import Login from '@/components/Login.vue';
+import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+import { extend, setInteractionMode } from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('digits', {
+  ...digits,
+  message: '{_field_} needs to be {length} digits. ({_value_})',
+})
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+})
+
+extend('max', {
+  ...max,
+  message: '{_field_} may not be greater than {length} characters',
+})
+
+extend('regex', {
+  ...regex,
+  message: '{_field_} {_value_} does not match {regex}',
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid',
+})
 
 export default Vue.extend({
   name: 'App',
